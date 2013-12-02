d72645bb99dbcfecef41409ab9891f651162cd85
parroit <andrea.parodi@ebansoftware.net>
2013-12-02 22:03:25 +0100

    subEach

--- test/simplator-subtemplates_test.js
+++ test/simplator-subtemplates_test.js
@@ -14,29 +14,67 @@ var expect = require("expect.js"),
 
 describe("simplator_subtemplates", function () {
     var results;
-    before(function () {
 
+    before(function () {
         subTemplates.use(templates);
 
-        var tmplt = simplator.compile("{name | sub('nameTmpl')}\n{address | sub('addressTmpl') }");
-
-        results = tmplt({
-            name: {
-                first: "Andrea",
-                last: "Parodi"
-            },
-            address: {
-                street: "via Casata",
-                city: "Genoa"
-            }
-        });
     });
 
     it("is defined", function () {
         expect(subTemplates).to.be.an('object');
     });
 
-    it("work!", function () {
-        expect(results).to.be.equal('Andrea,Parodi\nvia Casata,Genoa');
+    describe("object context", function () {
+        before(function () {
+
+
+            var tmplt = simplator.compile("{name | sub('nameTmpl')}\n{address | sub('addressTmpl') }");
+
+            results = tmplt({
+                name: {
+                    first: "Andrea",
+                    last: "Parodi"
+                },
+                address: {
+                    street: "via Casata",
+                    city: "Genoa"
+                }
+            });
+        });
+
+
+
+        it("work!", function () {
+            expect(results).to.be.equal('Andrea,Parodi\nvia Casata,Genoa');
+        });
+    });
+
+
+    describe("array context", function () {
+        before(function () {
+
+
+            var tmplt = simplator.compile("{name | sub('nameTmpl')}\n{addresses | subEach('addressTmpl','\t') }");
+
+            results = tmplt({
+                name: {
+                    first: "Andrea",
+                    last: "Parodi"
+                },
+                addresses: [{
+                    street: "via Casata",
+                    city: "Genoa"
+                },{
+                    street: "Another address",
+                    city: "Roma"
+                }]
+            });
+        });
+
+
+
+        it("work!", function () {
+            expect(results).to.be.equal('Andrea,Parodi\nvia Casata,Genoa\tAnother address,Roma');
+        });
     });
 });
