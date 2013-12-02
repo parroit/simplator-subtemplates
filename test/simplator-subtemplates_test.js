bc46c2866419605b4081985772e250ce6a74d273
parroit <andrea.parodi@ebansoftware.net>
2013-12-02 21:19:14 +0100

    completed... for now

--- test/simplator-subtemplates_test.js
+++ test/simplator-subtemplates_test.js
@@ -1,11 +1,42 @@
 'use strict';
 
-var expect = require("expect.js");
-var simplator_subtemplates = require("../lib/simplator-subtemplates");
+
+var expect = require("expect.js"),
+    simplator = require("simplator"),
+
+    templates = {
+        nameTmpl: simplator.compile("{first},{last}"),
+        addressTmpl: simplator.compile("{street},{city}")
+    },
+
+    subTemplates = require('../lib/simplator-subtemplates');
 
 
 describe("simplator_subtemplates", function () {
+    var results;
+    before(function () {
+
+        subTemplates.use(templates);
+
+        var tmplt = simplator.compile("{name | sub('nameTmpl')}\n{address | sub('addressTmpl') }");
+
+        results = tmplt({
+            name: {
+                first: "Andrea",
+                last: "Parodi"
+            },
+            address: {
+                street: "via Casata",
+                city: "Genoa"
+            }
+        });
+    });
+
     it("is defined", function () {
-        expect(simplator_subtemplates).to.be.an('object');
+        expect(subTemplates).to.be.an('object');
+    });
+
+    it("work!", function () {
+        expect(results).to.be.equal('Andrea,Parodi\nvia Casata,Genoa');
     });
 });
